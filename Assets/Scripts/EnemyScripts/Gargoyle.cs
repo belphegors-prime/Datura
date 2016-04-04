17c12a651a4428fabe510d19bb18c79a0dc15e10
David Blader <dblader79@gmail.com>
2016-04-04 07:53:43 -0500

    Added animator controllers for dungeon monsters

---
+++ Assets/Scripts/EnemyScripts/Gargoyle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gargoyle : Enemy
+{
+
+    protected override void Initialize()
+    {
+        // Character attributes
+        characterName = "Gargoyle";
+        maxHealth = 200.0f;
+        currentHealth = 200.0f;
+        attackSpeed = 5.0f;
+        damage = 5.0f;
+        patrolRange = 10.0f;
+        chaseRange = 12.0f;
+        attackRange = 5.0f;
+
+        // Set player reference and start AI
+        player = PlayerController.player;
+        ChangeState(activeState);
+    }
+}
