4b1423fc7111dcc0f478083306dffb5cb20941f2
David Blader <dblader79@gmail.com>
2016-04-04 05:34:54 -0400

    Added animator controllers for snow monsters

---
+++ Assets/Scripts/EnemyScripts/Lich.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lich : Enemy {
+    // Use this for initialization
+    protected override void Initialize()
+    {
+        // Character attributes
+        characterName = "Blizzard Lich";
+        maxHealth = 200.0f;
+        currentHealth = 200.0f;
+        attackSpeed = 5.0f;
+        damage = 5.0f;
+        patrolRange = 10.0f;
+        chaseRange = 12.0f;
+        attackRange = 3.0f;
+
+        // Set player reference and start AI
+        player = PlayerController.player;
+        ChangeState(activeState);
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/TaurenHunter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaurenHunter : Enemy
+{
+
+    protected override void Initialize()
+    {
+        // Character attributes
+        characterName = "Tauren Hunter";
+        maxHealth = 200.0f;
+        currentHealth = 200.0f;
+        attackSpeed = 5.0f;
+        damage = 5.0f;
+        patrolRange = 10.0f;
+        chaseRange = 12.0f;
+        attackRange = 5.0f;
+
+        // Set player reference and start AI
+        player = PlayerController.player;
+        ChangeState(activeState);
+    }
+}
